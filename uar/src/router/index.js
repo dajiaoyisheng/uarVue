2b4fe1e243fc06f6a20bf0b5c155d95945f65298
dajiaoyisheng <840758102@qq.com>
2017-07-26 15:29:42 +0800

    e

---
+++ uar/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// import Home from '../pages/home/index.vue'
+
+
+Vue.use(VueRouter)
+
+// export default new Router({
+//   //mode: 'history',
+//   routes: [
+//     //主页
+//     {
+//       path: '/',
+//       name: 'Home',
+//       component: Home
+//     }
+//   ]
+// })
+
+
